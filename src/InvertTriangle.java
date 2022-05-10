2dd0c1f9a56ac16a46a24bfbd4e7921c38f14624
Ziad Assem <ziad.assem2001@gmail.com>
2022-05-10 15:45:37 +0200

    invert triangle done

--- src/InvertTriangle.java
+++ src/InvertTriangle.java
@@ -1,49 +1,65 @@
 public class InvertTriangle {
+    static int moves=0;
+    
     public static void invert() {
-        String[][] x = new String[5][4];
+        
+        String[][] current = new String[5][5]; //creates starting coin array
         for (int i = 0; i < 4; i++) {
-            for (int j=0;j<4-i;j++){
+            for (int j = 0; j < 4 - i; j++) {
 
-                x[i][j]=" ";
+                current[i][j] = " ";
             }
-            for(int m=3-i;m<4;m++){
-
-                x[i][m]="o ";
+            for (int m = 3 - i; m < 4; m++) {
+                current[i][m] = "o ";
             }
-            System.out.println("");
-        }
-
-        for (int i = 0; i < 4; i++) {
-            for (int j=0;j<4;j++) {
-                System.out.print(x[i][j]);
-            }
-            System.out.println("");
         }
 
 
-        String[][] y = new String[5][5];
+        String[][] goal = new String[5][5]; //creates goal array
         for (int i = 1; i < 5; i++) {
             for (int j=0;j<i;j++){
 
-                y[i][j]=" ";
+                goal[i][j]=" ";
             }
             for(int m=i-1;m<4;m++){
 
-                y[i][m]="o ";
+                goal[i][m]="o ";
             }
-            System.out.println("");
+            
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j=0;j<4;j++) {
-                System.out.print(y[i][j]);
+        for (int i=0;i<4;i++) { //prints the starting array
+            for (int j = 0; j < 4; j++) {
+                System.out.print(current[i][j]);
             }
-            System.out.println("");
+            System.out.println();
         }
 
+        for (int i=0;i<5;i++){//each iteration
+            for (int j=0;j<5;j++){
+                if (goal[i][j]=="o "&& goal[i][j]!=current[i][j]){
+                    current[i][j]="o ";
+                    moves++;
+                }
+                if (current[i][j]=="o "&& goal[i][j]!=current[i][j]){
+                    current[i][j]=" ";
+                }
+            }
+        }
+        for (int i=0;i<5;i++) {
+            for (int j = 0; j < 4; j++) {
+                if (current[i][j]==null) {
+                   System.out.print(" ");
+                }else{
+                    System.out.print(current[i][j]);
+                }
+            }
+            System.out.println();
+        }
+        
     }
 
     public static void main(String[] args) {
-
-
+        InvertTriangle.invert();
+        System.out.println(InvertTriangle.moves);
     }
 }
