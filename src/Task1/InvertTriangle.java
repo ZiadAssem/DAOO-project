dc202de5b889387c1e5b9987d1ba315688a87095
Ziad Assem <ziad.assem2001@gmail.com>
2022-05-13 00:31:36 +0200

    added some refinements to task 1

--- src/Task1/InvertTriangle.java
+++ src/Task1/InvertTriangle.java
@@ -3,44 +3,43 @@ package Task1;
 public class InvertTriangle {
     static int moves=0;
     
-    public static void invert() {
+    public static void invert(int n) {//n is the number of coins in each edge
         
-        String[][] current = new String[5][5]; //creates starting coin array
+        String[][] current = new String[20][20]; //creates starting coin array
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4 - i; j++) {
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
                 current[i][j] = " ";
             }
-            for (int m = 3 - i; m < 4; m++) {
+            for (int m = n-1 - i; m < n; m++) {
                 current[i][m] = "o ";
             }
         }
 
 
-        String[][] goal = new String[5][5]; //creates goal array
+        String[][] goal = new String[20][20]; //creates goal array
 
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < n+1; i++) {
             for (int j=0;j<i;j++){
 
                 goal[i][j]=" ";
             }
-            for(int m=i-1;m<4;m++){
+            for(int m=i-1;m<n;m++){
 
                 goal[i][m]="o ";
             }
             
         }
 
-        for (int i=0;i<4;i++) { //prints the starting array
-            for (int j = 0; j < 4; j++) {
+        for (int i=0;i<n;i++) { //prints the starting array
+            for (int j = 0; j < n; j++) {
                 System.out.print(current[i][j]);
             }
             System.out.println();
         }
 
-        for (int i=0;i<5;i++){//each iteration
-            for (int j=0;j<5;j++){
+        for (int i=0;i<n+1;i++){//each iteration
+            for (int j=0;j<n+1;j++){
                 if (goal[i][j]=="o "&& goal[i][j]!=current[i][j]){
                     current[i][j]="o ";
                     moves++;
@@ -51,8 +50,8 @@ public class InvertTriangle {
             }
         }
 
-        for (int i=0;i<5;i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i=0;i<n+1;i++) {
+            for (int j = 0; j < n+1; j++) {
                 if (current[i][j]==null) {
                    System.out.print(" ");
                 }else{
@@ -65,7 +64,7 @@ public class InvertTriangle {
     }
 
     public static void main(String[] args) {
-        InvertTriangle.invert();
+        InvertTriangle.invert(4);
         System.out.println(InvertTriangle.moves);
     }
 }
